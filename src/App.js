e1eaab9dafd0b48e2b835c9c1ae72d775fc51089
majeedaisha <majeedaisha44@gmail.com>
2022-03-17 05:00:49 -0700

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Writers from "./writers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Writers Profile</h1>
+      <div className="container">
+        {Writers.map((writer) => (
+          <div key={writer.id} className="card">
+            <img
+              src={`images/${writer.avatar}.png`}
+              height="300px"
+              width="300px"
+              alt={writer.img}
+            />
+            <div className="textGroup">
+              <h3>{writer.name}</h3>
+              <p>{writer.email}</p>
+              <p>{writer.phone}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/writers.js
@@ -0,0 +1,39 @@
+const Writers=[
+    {
+        id:1,
+        name: "Jamila Lorde",
+        avatar:"audre_lorde",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    },
+    {
+        id:2,
+        name: "Jamila Lorde",
+        avatar:"du_bois",
+        email: "jamila23@gmail.com",
+        phone: "0202022415"
+    },
+    {
+        id:3,
+        name: "Jamila Lorde",
+        avatar:"belle_hooks",
+        email: "jamila23@gmail.com",
+        phone: "0270165233"
+    },
+    {
+        id:4,
+        name: "Jamila Lorde",
+        avatar:"maya_angelou",
+        email: "jamila23@gmail.com",
+        phone: "0503344565"
+    },
+    {
+        id:5,
+        name: "Jamila Lorde",
+        avatar:"james_baldwin",
+        email: "jamila23@gmail.com",
+        phone: "0540000000"
+    }
+]
+
+export default Writers;
